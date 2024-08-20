05e04af99ce0cc0717f0807283d43947dec152dd
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-20 20:33:51 +0500

    Create Models/WeatherData.cs : Location, Temperature, Condition, Humidity; Create External/HttpClients/WeatherHttpClient.cs : inject HttpClient, _apiKey, method FetchWeatherDataAsync(string location);

---
+++ WeatherAPIWrapper/External/HttpClients/WeatherHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using WeatherAPIWrapper.Models;
+
+namespace WeatherAPIWrapper.External.HttpClients
+{
+    public class WeatherHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public WeatherHttpClient(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _apiKey = config["WeatheAPI:Key"];
+        }
+
+        public async Task<WeatherData> FetchWeatherDataAsync(string location)
+        {
+            var response = await _httpClient.GetAsync($"v1/weather?location={location}&key={_apiKey}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<WeatherData>();
+        }
+    }
+}
---
+++ WeatherAPIWrapper/Models/WeatherData.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherAPIWrapper.Models
+{
+    public class WeatherData
+    {
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public string Condition { get; set; }
+        public double Humidity { get; set; }
+    }
+}
