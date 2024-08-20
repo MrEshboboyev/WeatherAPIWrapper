50ede6874ec87467c0e37981d87f4c1434ce20bc
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-20 20:38:58 +0500

    Create Services/IServices/IWeatherService.cs : GetWeatherDataAsync(string location); Create Services/WeatherService.cs : inject WeatherHttpClient and methods implemented;

---
+++ WeatherAPIWrapper/Services/IServices/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using WeatherAPIWrapper.Models;
+
+namespace WeatherAPIWrapper.Services.IServices
+{
+    public interface IWeatherService
+    {
+        Task<WeatherData> GetWeatherDataAsync(string location);
+    }
+}
---
+++ WeatherAPIWrapper/Services/WeatherService.cs
@@ -0,0 +1,21 @@
+﻿using WeatherAPIWrapper.External.HttpClients;
+using WeatherAPIWrapper.Models;
+using WeatherAPIWrapper.Services.IServices;
+
+namespace WeatherAPIWrapper.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private readonly WeatherHttpClient _weatherHttpClient;
+
+        public WeatherService(WeatherHttpClient weatherHttpClient)
+        {
+            _weatherHttpClient = weatherHttpClient;
+        }
+
+        public async Task<WeatherData> GetWeatherDataAsync(string location)
+        {
+            return await _weatherHttpClient.FetchWeatherDataAsync(location);
+        }
+    }
+}
