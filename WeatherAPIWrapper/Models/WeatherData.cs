88ad0605f021a0363413422aa4ac9dc78bcae87c
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-21 16:25:57 +0500

    WeatherData.cs : syntax error fixed in Longitude;

--- WeatherAPIWrapper/Models/WeatherData.cs
+++ WeatherAPIWrapper/Models/WeatherData.cs
@@ -5,7 +5,7 @@
         public string Address { get; set; }
         public string TimeZone { get; set; }
         public double Latitude { get; set; }
-        public double Longtitude { get; set; }
+        public double Longitude { get; set; }
         public string Description { get; set; }
         public DayData TodayWeather { get; set; }
     }
