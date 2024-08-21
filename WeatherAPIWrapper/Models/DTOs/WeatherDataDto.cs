ade519143b0882bf69293f57b530f170409e62ee
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-21 16:02:41 +0500

    Create Models/DTOs/WeatherDataDto.cs : Address, TimeZone, Latitude, Longitude, Decription, List<DayDataDto> Days and  DayDataDto class : DateTime, TempMax, TempMin, Conditions; WeatherData.cs : Address, TimeZone, Latitude, Longitude, Decription, DayData TodayWeather and  DayData class : DateTime, TempMax, TempMin, Conditions;

--- WeatherAPIWrapper/Models/DTOs/WeatherDataDto.cs
+++ WeatherAPIWrapper/Models/DTOs/WeatherDataDto.cs
@@ -1,21 +1,20 @@
-﻿namespace WeatherAPIWrapper.Models
+﻿namespace WeatherAPIWrapper.Models.DTOs
 {
-    public class WeatherData
+    public class WeatherDataDto
     {
         public string Address { get; set; }
-        public string TimeZone { get; set; }
+        public string Timezone { get; set; }
         public double Latitude { get; set; }
-        public double Longtitude { get; set; }
+        public double Longitude { get; set; }
         public string Description { get; set; }
-        public DayData TodayWeather { get; set; }
+        public List<DayDataDto> Days { get; set; }
     }
 
-    public class DayData
+    public class DayDataDto
     {
         public string DateTime { get; set; }
         public double TempMax { get; set; }
         public double TempMin { get; set; }
         public string Conditions { get; set; }
     }
-
 }
